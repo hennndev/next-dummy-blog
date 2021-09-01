55fdfe29f43cdc1721283c04cab8a1c884fe532c
hennndev <hendraputra230897@gmail.com>
2021-09-01 22:00:53 +0700

    done

--- components/layout/navbar.js
+++ components/layout/navbar.js
@@ -1,20 +1,21 @@
 import Link from 'next/link'
 import { MenuAlt3Icon } from '@heroicons/react/outline'
 import { useState } from 'react'
+import { useRouter } from 'next/router'
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false)
-    const handleClose = () => setIsOpen(false)
+    const router = useRouter()
 
     return (
         <header className="lg:container mx-auto p-5">
             <div className="flex justify-between items-center">
                 <div>
-                    <h1 className="text-xl font-bold">MyBlogs</h1>
+                    <h1 className="text-xl font-bold cursor-pointer" onClick={() => router.push('/')}>MyBlogs</h1>
                 </div>
                 <nav>
-                    <ul className={`fixed w-full h-full left-0 ${!isOpen ? '-top-full' : 'top-0'} duration-700 z-40 bg-gray-300 justify-center flex-col
-                    sm:static sm:bg-transparent flex sm:flex-row sm:text-gray-700 jusitfy-between items-center`}>
+                    <ul className={`fixed w-full h-full left-0 ${!isOpen ? '-top-full' : 'top-0'} duration-700 z-40 bg-gray-300 pt-52 flex-col 
+                    sm:static sm:p-0 sm:bg-transparent flex sm:flex-row sm:text-gray-700 jusitfy-between items-center`}>
                         <li className="px-2 text-md mb-4 text-xl sm:text-base sm:mb-0" onClick={() => setIsOpen(false)}>
                             <Link href="/blogs">
                                 <a>Blogs</a>
@@ -32,7 +33,7 @@ const Navbar = () => {
                         </li>
                     </ul>
                 </nav>
-                <MenuAlt3Icon className="block h-5 z-50 sm:hidden cursor-pointer" onClick={() => setIsOpen(!isOpen)}/>
+                <MenuAlt3Icon className={`block h-5 z-50 ${isOpen && 'fixed'} ${isOpen && 'right-5'} sm:hidden cursor-pointer`} onClick={() => setIsOpen(!isOpen)}/>
             </div>
         </header>
     )
--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,7 +1,17 @@
+import { useRouter } from "next/router"
+
 const Contact = () => {
+    const router = useRouter()
+
     return (
-        <div>
-            <h1>Contact Page</h1>
+        <div className="ml-5 mt-10 mb-10 sm:container sm:ml-16">
+            <h1 className="font-bold text-lg mb-5">Contact Me</h1>
+            <p className="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus semper tempus feugiat. Morbi posuere metus eget eros dignissim, non hendrerit tellus dapibus. Sed euismod metus nisl. Donec id nunc sit amet ante semper dictum id facilisis lectus. Praesent quam ante, fringilla quis risus vitae, faucibus viverra nisi. Duis ut tempor nisi. Suspendisse tortor mauris, interdum vitae volutpat vel, tincidunt a erat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc rhoncus aliquet diam, sit amet efficitur orci dictum eu. Nunc laoreet ante eget lorem gravida interdum. Aenean aliquam mi lectus, non elementum lacus sagittis aliquam. Cras et diam rhoncus, convallis arcu nec, aliquet arcu. Duis nec metus tempor neque faucibus hendrerit sit amet id libero. Cras posuere est fringilla congue dictum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae Donec eros lacus, pretium at tortor quis, dapibus viverra magna. Aenean arcu diam, posuere vel nunc dapibus, tempus hendrerit sapien. Aenean sodales semper dolor, dignissim luctus libero dictum vitae. Nunc eu est sed arcu aliquet vulputate. Ut a ultricies justo. Praesent sagittis molestie leo, id accumsan justo scelerisque vitae. Nulla facilisi. Suspendisse eu lorem condimentum, tempus lectus nec, elementum urna. Proin pretium massa lacus.</p>
+            <div className="mt-4">
+                <button onClick={() => router.push('/')} 
+                    className="bg-gray-600 hover:bg-gray-700 px-5 py-2 rounded-md text-white cursor-pointer">Back Home
+                </button>
+            </div>
         </div>
     )
 }
